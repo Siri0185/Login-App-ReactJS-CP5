0f57ce46336ab02ccbde5308ab16ef57723c6d1c
srilakshmi <srilakshmir8@gmail.com>
2022-09-10 14:10:49 +0000

    Login App

---
+++ src/components/Message/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const Message = props => {
+  const {login} = props
+  return login ? (
+    <h1 className="message">Welcome User</h1>
+  ) : (
+    <h1 className="message">Please Login</h1>
+  )
+}
+
+export default Message
